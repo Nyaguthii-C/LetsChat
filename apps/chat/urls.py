5b2969f3dbd7c0018dc6861d044f8e000abe1d0d
Nyaguthii-C <nyaguthiigc@gmail.com>
2025-04-23 19:22:13 +0300

    modify url for uniformity

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import MessageCreateView, MessageDeleteView, MessageUpdateView, ConversationListCreateView, ConversationDetailView
 
 urlpatterns = [
-    path('messages/send/', MessageCreateView.as_view(), name='send-message'),
+    path('messages/send/', MessageCreateView.as_view(), name='message-send'),
     path('messages/<int:pk>/delete/', MessageDeleteView.as_view(), name='message-delete'),
-    path('messages/<int:pk>/edit/', MessageUpdateView.as_view(), name='message-update'),
+    path('messages/<int:pk>/update/', MessageUpdateView.as_view(), name='message-update'),
     path('conversations/all/', ConversationListCreateView.as_view(), name='conversation-list-create'),
     path('conversations/<int:pk>/', ConversationDetailView.as_view(), name='conversation-detail'),
 ]
