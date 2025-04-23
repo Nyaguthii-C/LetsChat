6b917583f2cf25bb05b34ef70472185cb516bfd6
Nyaguthii-C <nyaguthiigc@gmail.com>
2025-04-24 01:54:30 +0300

    add admin dashboard

--- apps/chat/admin.py
+++ apps/chat/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Conversation, Message, Reaction
 
-# Register your models here.
+
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_at', 'participant_list')
+    search_fields = ('participants__email',)
+    list_filter = ('created_at',)
+
+    def participant_list(self, obj):
+        return ", ".join(user.email for user in obj.participants.all())
+    participant_list.short_description = 'Participants'
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'conversation', 'sender', 'receiver', 'content_preview', 'timestamp', 'is_read')
+    search_fields = ('sender__email', 'receiver__email', 'content')
+    list_filter = ('timestamp', 'is_read')
+
+    def content_preview(self, obj):
+        return obj.content[:40] + "..." if len(obj.content) > 40 else obj.content
+    content_preview.short_description = 'Content Preview'
+
+
+@admin.register(Reaction)
+class ReactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'message', 'user', 'emoji')
+    search_fields = ('user__email', 'emoji', 'message__content')
+    list_filter = ('emoji',)
+
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(BaseUserAdmin):
+    model = CustomUser
+    list_display = ('email', 'full_name', 'is_staff', 'is_superuser', 'date_joined')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('email', 'full_name')
+    ordering = ('-date_joined',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('full_name', 'profile_photo')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'full_name', 'password1', 'password2', 'is_staff', 'is_superuser'),
+        }),
+    )
+
+    filter_horizontal = ('groups', 'user_permissions')
+
