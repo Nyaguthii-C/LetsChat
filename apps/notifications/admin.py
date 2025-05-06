5bc269aa1658fc69e006b68ec9a0f3e339cc29de
Nyaguthii-C <nyaguthiigc@gmail.com>
2025-05-06 15:49:37 +0300

    register notifications on admin dash

--- apps/notifications/admin.py
+++ apps/notifications/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'notification_type', 'message', 'is_seen', 'created_at')
+    list_filter = ('notification_type', 'is_seen', 'created_at')
+    search_fields = ('user__email', 'message__content')
+    date_hierarchy = 'created_at'
