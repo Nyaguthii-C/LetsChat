493330855c7fe8c808640a41295a0c996a217e04
Nyaguthii-C <nyaguthiigc@gmail.com>
2025-05-09 16:39:05 +0300

    add notification id to listed fields

--- apps/notifications/admin.py
+++ apps/notifications/admin.py
@@ -3,7 +3,7 @@ from .models import Notification
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('user', 'notification_type', 'message', 'is_seen', 'created_at')
+    list_display = ('id', 'user', 'notification_type', 'message', 'is_seen', 'created_at')
     list_filter = ('notification_type', 'is_seen', 'created_at')
     search_fields = ('user__email', 'message__content')
     date_hierarchy = 'created_at'
