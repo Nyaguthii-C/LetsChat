b2ac6ae444f0ea15bfd1d1f2273baebbc66ab5d3
Nyaguthii-C <nyaguthiigc@gmail.com>
2025-05-23 17:05:07 +0300

    link reaction to notification model

--- apps/notifications/models.py
+++ apps/notifications/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from apps.chat.models import Message
+from apps.chat.models import Message, Reaction
 
 class Notification(models.Model):
     NOTIF_TYPE_CHOICES = [
@@ -10,8 +10,9 @@ class Notification(models.Model):
         ("typing", "Typing"),
     ]
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # to be notified
     message = models.ForeignKey(Message, on_delete=models.CASCADE, null=True, blank=True)
+    reaction = models.ForeignKey(Reaction, on_delete=models.CASCADE, null=True, blank=True)
     notification_type = models.CharField(max_length=20, choices=NOTIF_TYPE_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     is_seen = models.BooleanField(default=False)
